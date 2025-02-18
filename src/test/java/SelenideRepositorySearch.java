5981585ab4f2dd637df5813f03cd65669f2907b6
Caban <awerinzh@yandex.ru>
2025-02-18 19:13:22 +0300

    Test new

---
+++ src/test/java/SelenideRepositorySearch.java
@@ -0,0 +1,49 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SelenideRepositorySearch {
+
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop() {
+
+        Configuration.holdBrowserOpen = true;
+        //Открыть гитхаб
+        open("https://github.com/");
+        //Нажать на Sign in
+        $(".HeaderMenu-link-wrap").click();
+        //Ввести почту для входа
+        $("#login_field").setValue("awerinzh@yandex.ru");
+        //Ввести пароль для входа
+        $("#password").setValue("averin228337");
+        //Нажать на Sign in
+        $("[value='Sign in']").click();
+        //Нажать на поле ввода
+        $(".placeholder").click();
+        //Ввести в поиск "selenide" и нажать Enter
+        $("#query-builder-test").setValue("selenide").pressEnter();
+        //Нажать на первую ссылку
+        $(".search-title").click();
+        //Сделать проверку надписи "selenide/selenide"
+        $(".position-relative").shouldHave(text("selenide / selenide"));
+        //Подвести мышку к первому аватару из блока contributors
+        $(".BorderGrid").$(byText("Contributors")).ancestor(".BorderGrid-row").$$("ul li").first().hover();
+        //
+        $(".Truncate-text--expandable").shouldHave(text("Andrei Solntsev"));
+        //
+        $(".AppHeader-user").click();
+        //
+        $x("//span[text()='Sign out']").click();
+        //
+        $(".inline-form").click();
+
+
+    }
+
+
+
+}
